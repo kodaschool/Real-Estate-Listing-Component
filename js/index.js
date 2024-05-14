74ad69065d4bed3f121814b190fbd2757cb4b5d2
Mary <marieshix98@gmail.com>
2024-05-14 23:41:34 +0300

    refactor minor changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
-// Array of Font Awesome icon classes
 const iconClasses = ["fa-star", "fa-star", "fa-star", "fa-star", "fa-star"];
-// Get the container element
+
 const iconContainer = document.querySelector(".review_icons_container");
 iconClasses.forEach((iconClass) => {
   const iconElement = document.createElement("i");
